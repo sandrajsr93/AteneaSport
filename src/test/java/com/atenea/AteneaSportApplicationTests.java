c3588e8d8e7a2e27859ee2b4bc0a99aaa79a594b
sandrajsr <sandra.juan@nfq.es>
2020-03-13 12:17:35 +0100

    Subida de proyecto a git

---
+++ src/test/java/com/atenea/AteneaSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atenea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteneaSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
